67ee18e39ef96930450bf4863a474b0f52dedcda
zghaia@gmail.com <zghaia@gmail.com@738dce7f-4062-a692-ef20-340796713741>
2012-06-09 13:35:19 +0000

    加入登陆注销处理代码

---
+++ Dr.com/ZghaiaDrcom.m
@@ -0,0 +1,33 @@
+//
+//  Zghaia_Dr_com.m
+//  Dr.com for IOS
+//
+//  Created by zghaia@gmail.com on 6/8/12.
+//  Copyright (c) 2012 UESTC. All rights reserved.
+//
+
+#import "ZghaiaDrcom.h"
+
+@implementation ZghaiaDrcom
+
++(void)loginOnSever:(NSString*)server userName:(NSString*)user  password:(NSString*)password
+{
+    NSMutableURLRequest*  request = [NSMutableURLRequest new];  
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://%@/", server]]];  
+    [request setHTTPMethod:@"POST"];  
+    [request addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];  
+    [request setHTTPBody:[NSString stringWithFormat:@"DDDDD=%@&upass=%@&0MKKey=%B5%C7%C2%BC+Login", user, password]];  
+    NSData* data =  [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];  
+    NSLog(@"%@",[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+}
+
++(void)logoutOnSever:(NSString*)server
+{
+    NSMutableURLRequest*  request = [NSMutableURLRequest new];  
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://%@/F.htm", server]]];  
+    [request setHTTPMethod:@"GET"];  
+    NSData* data =  [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];  
+    NSLog(@"%@",[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+}
+
+@end
